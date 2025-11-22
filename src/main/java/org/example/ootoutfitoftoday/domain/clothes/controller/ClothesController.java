09920e38242692eb46ae5c049c195f765b062f93
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-11-22 22:31:37 +0900

    [feat] clothes: 컨트롤러의 스웨거 인터페이스로 분리

--- src/main/java/org/example/ootoutfitoftoday/domain/clothes/controller/ClothesController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothes/controller/ClothesController.java
@@ -8,30 +8,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.example.ootoutfitoftoday.common.response.Response;
 import org.example.ootoutfitoftoday.common.response.SliceResponse;
 import org.example.ootoutfitoftoday.domain.auth.dto.AuthUser;
 import org.example.ootoutfitoftoday.domain.clothes.dto.request.ClothesImageUnlinkRequest;
 import org.example.ootoutfitoftoday.domain.clothes.dto.request.ClothesRequest;
 import org.example.ootoutfitoftoday.domain.clothes.dto.response.ClothesResponse;
-import org.example.ootoutfitoftoday.domain.clothes.exception.ClothesSuccessCode;
-import org.example.ootoutfitoftoday.domain.clothes.service.command.ClothesCommandService;
-import org.example.ootoutfitoftoday.domain.clothes.service.query.ClothesQueryService;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "옷 관리", description = "옷관련 API")
 @SecurityRequirement(name = "bearerAuth")
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/v1/clothes")
-public class ClothesController {
-
-    private final ClothesCommandService clothesCommandService;
-    private final ClothesQueryService clothesQueryService;
+public interface ClothesController {
 
     @Operation(
             summary = "옷 등록",
@@ -42,15 +33,10 @@ public class ClothesController {
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
                     @ApiResponse(responseCode = "404", description = "찾을 수 없음")
             })
-    @PostMapping
-    public ResponseEntity<Response<ClothesResponse>> createClothes(
+    ResponseEntity<Response<ClothesResponse>> createClothes(
             @AuthenticationPrincipal AuthUser authUser,
             @Valid @RequestBody ClothesRequest clothesRequest
-    ) {
-        ClothesResponse clothesResponse = clothesCommandService.createClothes(authUser.getUserId(), clothesRequest);
-
-        return Response.success(clothesResponse, ClothesSuccessCode.CLOTHES_CREATED);
-    }
+    );
 
     @Operation(
             summary = "옷 전체 조회",
@@ -59,26 +45,14 @@ public class ClothesController {
                     @ApiResponse(responseCode = "200", description = "조회 성공"),
                     @ApiResponse(responseCode = "401", description = "인증 실패")
             })
-    @GetMapping
-    public ResponseEntity<SliceResponse<ClothesResponse>> getClothes(
+    ResponseEntity<SliceResponse<ClothesResponse>> getClothes(
             @AuthenticationPrincipal AuthUser authUser,
             @Parameter(description = "카테고리 ID") @RequestParam(required = false) Long categoryId,
             @Parameter(description = "옷 색상") @RequestParam(required = false) ClothesColor clothesColor,
             @Parameter(description = "옷 사이즈") @RequestParam(required = false) ClothesSize clothesSize,
             @Parameter(description = "마지막 조회 옷 아이디") @RequestParam(required = false) Long lastClothesId,
             @Parameter(description = "페이지당 개수") @RequestParam(defaultValue = "10") int size
-    ) {
-        Slice<ClothesResponse> clothes = clothesQueryService.getClothes(
-                authUser.getUserId(),
-                categoryId,
-                clothesColor,
-                clothesSize,
-                lastClothesId,
-                size
-        );
-
-        return SliceResponse.success(clothes, ClothesSuccessCode.CLOTHES_OK);
-    }
+    );
 
     @Operation(
             summary = "해당 옷 조회",
@@ -88,15 +62,10 @@ public class ClothesController {
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
                     @ApiResponse(responseCode = "404", description = "찾을 수 없음")
             })
-    @GetMapping("/{clothesId}")
-    public ResponseEntity<Response<ClothesResponse>> getClothesById(
+    ResponseEntity<Response<ClothesResponse>> getClothesById(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long clothesId
-    ) {
-        ClothesResponse clothesResponse = clothesQueryService.getClothesById(authUser.getUserId(), clothesId);
-
-        return Response.success(clothesResponse, ClothesSuccessCode.CLOTHES_OK);
-    }
+    );
 
     @Operation(
             summary = "해당 옷 수정",
@@ -107,16 +76,11 @@ public class ClothesController {
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
                     @ApiResponse(responseCode = "404", description = "찾을 수 없음")
             })
-    @PutMapping("/{clothesId}")
-    public ResponseEntity<Response<ClothesResponse>> updateClothes(
+    ResponseEntity<Response<ClothesResponse>> updateClothes(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long clothesId,
             @Valid @RequestBody ClothesRequest clothesRequest
-    ) {
-        ClothesResponse clothesResponse = clothesCommandService.updateClothes(authUser.getUserId(), clothesId, clothesRequest);
-
-        return Response.success(clothesResponse, ClothesSuccessCode.CLOTHES_UPDATE);
-    }
+    );
 
     @Operation(
             summary = "해당 옷 삭제",
@@ -126,15 +90,10 @@ public class ClothesController {
                     @ApiResponse(responseCode = "401", description = "인증 실패"),
                     @ApiResponse(responseCode = "404", description = "찾을 수 없음")
             })
-    @DeleteMapping("/{clothesId}")
-    public ResponseEntity<Response<Void>> deleteClothes(
-            @AuthenticationPrincipal AuthUser authUser,
-            @PathVariable Long clothesId
-    ) {
-        clothesCommandService.deleteClothes(authUser.getUserId(), clothesId);
-
-        return Response.success(null, ClothesSuccessCode.CLOTHES_DELETE);
-    }
+    ResponseEntity<Response<Void>> deleteClothes(
+            AuthUser authUser,
+            Long clothesId
+    );
 
     @Operation(
             summary = "해당 옷에 등록된 이미지 제거",
@@ -142,18 +101,9 @@ public class ClothesController {
             responses = {
                     @ApiResponse(responseCode = "200", description = "삭제 성공")
             })
-    @PostMapping("/{clothesId}/images/remove")
-    public ResponseEntity<Response<Void>> removeClothesImages(
+    ResponseEntity<Response<Void>> removeClothesImages(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long clothesId,
             @RequestBody ClothesImageUnlinkRequest clothesImageUnlinkRequest
-    ) {
-        clothesCommandService.removeClothesImages(
-                authUser.getUserId(),
-                clothesId,
-                clothesImageUnlinkRequest
-        );
-
-        return Response.success(null, ClothesSuccessCode.CLOTHES_IMAGE_REMOVE);
-    }
+    );
 }

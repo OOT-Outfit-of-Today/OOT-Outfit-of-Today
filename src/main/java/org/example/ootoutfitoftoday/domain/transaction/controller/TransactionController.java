668f1e8b1a52b461d76714b17e8530609121a4fb
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-11-23 23:11:19 +0900

    [feat] transaction: 컨트롤러의 스웨거 인터페이스로 분리

--- src/main/java/org/example/ootoutfitoftoday/domain/transaction/controller/TransactionController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/transaction/controller/TransactionController.java
@@ -5,30 +5,22 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.example.ootoutfitoftoday.common.response.Response;
 import org.example.ootoutfitoftoday.domain.auth.dto.AuthUser;
-import org.example.ootoutfitoftoday.domain.payment.exception.PaymentSuccessCode;
 import org.example.ootoutfitoftoday.domain.transaction.dto.request.TransactionConfirmRequest;
 import org.example.ootoutfitoftoday.domain.transaction.dto.request.TransactionRequest;
 import org.example.ootoutfitoftoday.domain.transaction.dto.response.TransactionAcceptResponse;
 import org.example.ootoutfitoftoday.domain.transaction.dto.response.TransactionCancelResponse;
 import org.example.ootoutfitoftoday.domain.transaction.dto.response.TransactionCompleteResponse;
 import org.example.ootoutfitoftoday.domain.transaction.dto.response.TransactionResponse;
-import org.example.ootoutfitoftoday.domain.transaction.exception.TransactionSuccessCode;
-import org.example.ootoutfitoftoday.domain.transaction.service.command.TransactionCommandService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "거래 관리", description = "거래 관련 API")
 @SecurityRequirement(name = "bearerAuth")
-@RestController
-@RequestMapping("/v1/transactions")
-@RequiredArgsConstructor
-public class TransactionController {
-
-    private final TransactionCommandService transactionCommandService;
+public interface TransactionController {
 
     @Operation(
             summary = "거래 요청",
@@ -40,18 +32,10 @@ public class TransactionController {
                     @ApiResponse(responseCode = "409", description = "리소스 충돌")
             }
     )
-    @PostMapping("/request")
-    public ResponseEntity<Response<TransactionResponse>> requestTransaction(
+    ResponseEntity<Response<TransactionResponse>> requestTransaction(
             @AuthenticationPrincipal AuthUser authUser,
             @Valid @RequestBody TransactionRequest request
-    ) {
-        TransactionResponse response = transactionCommandService.requestTransaction(
-                authUser.getUserId(),
-                request
-        );
-
-        return Response.success(response, TransactionSuccessCode.TRANSACTION_REQUESTED);
-    }
+    );
 
     @Operation(
             summary = "결제 승인",
@@ -65,20 +49,11 @@ public class TransactionController {
                     @ApiResponse(responseCode = "408", description = "요청 시간 초과")
             }
     )
-    @PostMapping("/{transactionId}/confirm")
-    public ResponseEntity<Response<TransactionResponse>> confirmTransaction(
+    ResponseEntity<Response<TransactionResponse>> confirmTransaction(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long transactionId,
             @Valid @RequestBody TransactionConfirmRequest request
-    ) {
-        TransactionResponse response = transactionCommandService.confirmTransaction(
-                authUser.getUserId(),
-                transactionId,
-                request
-        );
-
-        return Response.success(response, PaymentSuccessCode.PAYMENT_APPROVED);
-    }
+    );
 
     @Operation(
             summary = "거래 수락",
@@ -91,18 +66,10 @@ public class TransactionController {
                     @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
             }
     )
-    @PostMapping("/{transactionId}/accept")
-    public ResponseEntity<Response<TransactionAcceptResponse>> acceptTransaction(
+    ResponseEntity<Response<TransactionAcceptResponse>> acceptTransaction(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long transactionId
-    ) {
-        TransactionAcceptResponse response = transactionCommandService.acceptTransaction(
-                authUser.getUserId(),
-                transactionId
-        );
-
-        return Response.success(response, TransactionSuccessCode.TRANSACTION_ACCEPTED);
-    }
+    );
 
     @Operation(
             summary = "거래 확정",
@@ -115,18 +82,10 @@ public class TransactionController {
                     @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
             }
     )
-    @PostMapping("/{transactionId}/complete")
-    public ResponseEntity<Response<TransactionCompleteResponse>> completeTransaction(
+    ResponseEntity<Response<TransactionCompleteResponse>> completeTransaction(
             @PathVariable Long transactionId,
             @AuthenticationPrincipal AuthUser authUser
-    ) {
-        TransactionCompleteResponse response = transactionCommandService.completeTransaction(
-                authUser.getUserId(),
-                transactionId
-        );
-
-        return Response.success(response, TransactionSuccessCode.TRANSACTION_COMPLETED);
-    }
+    );
 
     @Operation(
             summary = "구매자 취소",
@@ -139,16 +98,8 @@ public class TransactionController {
                     @ApiResponse(responseCode = "404", description = "요청 리소스를 찾을 수 없음")
             }
     )
-    @PostMapping("/{transactionId}/cancel-buyer")
-    public ResponseEntity<Response<TransactionCancelResponse>> cancelByBuyer(
+    ResponseEntity<Response<TransactionCancelResponse>> cancelByBuyer(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long transactionId
-    ) {
-        TransactionCancelResponse response = transactionCommandService.cancelByBuyer(
-                authUser.getUserId(),
-                transactionId
-        );
-
-        return Response.success(response, TransactionSuccessCode.TRANSACTION_CANCELLED);
-    }
+    );
 }

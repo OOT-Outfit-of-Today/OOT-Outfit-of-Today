0fcee06c7d20fba9c1b50b9f16e87124e48e71a7
cjn404 <moi404@naver.com>
2025-11-28 01:06:53 +0900

    [feat] init: AWS_REGION 환경변수 명시적으로 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,8 +32,14 @@ CMDS=(
   "docker pull ${FULL_URI}"
   "docker stop ${CONTAINER_NAME} || true"
   "docker rm   ${CONTAINER_NAME} || true"
-  "docker run -d --name ${CONTAINER_NAME} --restart=always -p ${APP_PORT}:${APP_PORT} -e SPRING_PROFILES_ACTIVE=${SPRING_PROFILE} ${FULL_URI}"
-)
+  "docker run -d \\
+      --name ${CONTAINER_NAME} \\
+      --restart=always \\
+      -p ${APP_PORT}:${APP_PORT} \\
+      -e SPRING_PROFILES_ACTIVE=${SPRING_PROFILE} \\
+      -e AWS_REGION=${AWS_REGION} \\
+      ${FULL_URI}"
+  )
 
 # Bash 배열 → JSON 배열 변환 (jq 필수)
 COMMANDS_JSON=$(jq -Rn --argjson arr "$(printf '%s\n' "${CMDS[@]}" | jq -R . | jq -s .)" '$arr')

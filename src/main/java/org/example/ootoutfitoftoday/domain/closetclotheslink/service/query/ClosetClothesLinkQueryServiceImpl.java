a65e2034913937595fb171aea2766a4f6ce95dc7
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-06 15:46:05 +0900

    [refactor] closetClothesLink: 조건문으로 권한을 검증하는 것이 아닌 조회할 때 유저의 권한과 데이터 존재 여부를 판단하여 가져올 수 있도록 리팩토링 진행했음.

--- src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/service/query/ClosetClothesLinkQueryServiceImpl.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/service/query/ClosetClothesLinkQueryServiceImpl.java
@@ -1,12 +1,9 @@
 package org.example.ootoutfitoftoday.domain.closetclotheslink.service.query;
 
 import lombok.RequiredArgsConstructor;
-import org.example.ootoutfitoftoday.domain.closet.entity.Closet;
 import org.example.ootoutfitoftoday.domain.closet.service.query.ClosetQueryService;
 import org.example.ootoutfitoftoday.domain.closetclotheslink.dto.response.ClosetClothesLinkGetResponse;
 import org.example.ootoutfitoftoday.domain.closetclotheslink.entity.ClosetClothesLink;
-import org.example.ootoutfitoftoday.domain.closetclotheslink.exception.ClosetClothesLinkErrorCode;
-import org.example.ootoutfitoftoday.domain.closetclotheslink.exception.ClosetClothesLinkException;
 import org.example.ootoutfitoftoday.domain.closetclotheslink.repository.ClosetClothesLinkRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -52,11 +47,7 @@ public class ClosetClothesLinkQueryServiceImpl implements ClosetClothesLinkQuery
             String sort,
             String direction
     ) {
-        Closet closet = closetQueryService.findClosetById(closetId);
-
-        if (!Objects.equals(closet.getUserId(), userId)) {
-            throw new ClosetClothesLinkException(ClosetClothesLinkErrorCode.CLOSET_CLOTHES_FORBIDDEN);
-        }
+        closetQueryService.findClosetByIdAndUserIdAndIsDeletedFalse(userId, closetId);
 
         Pageable pageable = createPageable(
                 page,

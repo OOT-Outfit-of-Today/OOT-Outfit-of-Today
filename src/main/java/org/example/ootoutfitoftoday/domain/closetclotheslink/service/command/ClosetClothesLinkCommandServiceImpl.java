4275ad29c030c187783d416239bfb3453647f8d2
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-06 15:44:55 +0900

    [fix] closetClothesLink: 한 번이라도 옷장에 등록한 옷이라면 다시 등록할 수가 없는 문제점 발견하여 수정함
    - 데이터를 Optional로 조회 후 조건문으로 분기점을 구현하여 데이터 존재 여부에 따른 각각의 로직이 실행될 수 있도록 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/service/command/ClosetClothesLinkCommandServiceImpl.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closetclotheslink/service/command/ClosetClothesLinkCommandServiceImpl.java
@@ -15,6 +15,8 @@ import org.example.ootoutfitoftoday.domain.clothes.service.query.ClothesQuerySer
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -30,13 +32,31 @@ public class ClosetClothesLinkCommandServiceImpl implements ClosetClothesLinkCom
             Long closetId,
             ClosetClothesLinkRequest request
     ) {
-        Closet closet = closetQueryService.findClosetByIdAndUserIdAndIsDeletedFalse(closetId, userId);
-        Clothes clothes = clothesQueryService.findClothesByIdAndUserIdAndIsDeletedFalse(request.clothesId(), userId);
+        // 로그인 유저를 기준으로 옷장과 옷을 조회하기에 유저 자체를 검증하는 조건문이 필요없음.
+        Closet closet = closetQueryService.findClosetByIdAndUserIdAndIsDeletedFalse(userId, closetId);
+        Clothes clothes = clothesQueryService.findClothesByIdAndUserIdAndIsDeletedFalse(userId, request.clothesId());
 
-        if (closetClothesLinkRepository.existsByClosetIdAndClothesId(closetId, request.clothesId())) {
-            throw new ClosetClothesLinkException(ClosetClothesLinkErrorCode.CLOSET_CLOTHES_ALREADY_LINKED);
+        // 데이터가 존재할 수도 있고 존재하지 않을 수도 있기에 Optional로 구현
+        Optional<ClosetClothesLink> existing = closetClothesLinkRepository.findByClosetIdAndClothesId(closetId, request.clothesId());
+
+        // 데이터가 존재한다면?
+        if (existing.isPresent()) {
+            // 데이터를 변수에 담는다.
+            ClosetClothesLink alreadyLinked = existing.get();
+
+            // 삭제되지 않은 데이터라면?
+            if (!alreadyLinked.isDeleted()) {
+                throw new ClosetClothesLinkException(ClosetClothesLinkErrorCode.CLOSET_CLOTHES_ALREADY_LINKED);
+            }
+
+            // 삭제되지 않은 상태로 변환
+            alreadyLinked.restore();
+
+            // 결과를 여기서 반환해서 로직 종료
+            return ClosetClothesLinkResponse.from(alreadyLinked);
         }
 
+        // 아래는 DB에 입력된 데이터가 존재하지 않을 경우 새로 insert 쿼리를 사용하여 새로운 데이터 생성하는 로직
         ClosetClothesLink link = ClosetClothesLink.create(closet, clothes);
         ClosetClothesLink savedLink = closetClothesLinkRepository.save(link);
 
@@ -49,11 +69,11 @@ public class ClosetClothesLinkCommandServiceImpl implements ClosetClothesLinkCom
             Long closetId,
             Long clothesId
     ) {
-        closetQueryService.findClosetByIdAndUserIdAndIsDeletedFalse(closetId, userId);
+        closetQueryService.findClosetByIdAndUserIdAndIsDeletedFalse(userId, closetId);
 
         ClosetClothesLink link = closetClothesLinkRepository.findByClosetIdAndClothesIdAndIsDeletedFalse(closetId, clothesId).orElseThrow(
-                        () -> new ClosetClothesLinkException(ClosetClothesLinkErrorCode.CLOTHES_NOT_LINKED)
-                );
+                () -> new ClosetClothesLinkException(ClosetClothesLinkErrorCode.CLOTHES_NOT_LINKED)
+        );
 
         link.softDelete();
 

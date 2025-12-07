a279b2aa73af3ae36ec9fbc9e58057087a587447
cjn404 <moi404@naver.com>
2025-12-07 23:21:14 +0900

    [feat] redis: 메인 클래스 내 ConfigurationPropertiesScan 활성화
    - @ConfigurationProperties 자동 스캔 설정
    - @Component는 일반 빈용으로만,
    - @ConfigurationPropertiesScan로 Properties 전용
    -> 의도 명확성, 관심사 분리

--- src/main/java/org/example/ootoutfitoftoday/OotOutfitOfTodayApplication.java
+++ src/main/java/org/example/ootoutfitoftoday/OotOutfitOfTodayApplication.java
@@ -3,8 +3,10 @@ package org.example.ootoutfitoftoday;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 @EntityScan(basePackages = {
         "org.example.ootoutfitoftoday",   // 기존 엔티티

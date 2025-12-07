cd51a8d998943e31a610f448c100fe975e0c3146
cjn404 <moi404@naver.com>
2025-12-08 00:25:58 +0900

    [chore] redisson: 사용하지 않는 import 문 삭제

--- src/main/java/org/example/ootoutfitoftoday/common/config/RedissonProperties.java
+++ src/main/java/org/example/ootoutfitoftoday/common/config/RedissonProperties.java
@@ -3,7 +3,6 @@ package org.example.ootoutfitoftoday.common.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * Redisson 설정 Properties

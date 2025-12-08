3e9a9a3e6c18106753c6dd50c9bcd6361080a233
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-08 21:00:27 +0900

    [feat] wearRecord: baseDate의 기준 값을 요청으로 받기위한 dto 수정

--- src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/dto/request/WearRecordCreateRequest.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/dto/request/WearRecordCreateRequest.java
@@ -3,10 +3,15 @@ package org.example.ootoutfitoftoday.domain.wearrecord.dto.request;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record WearRecordCreateRequest(
 
         @NotNull(message = "옷 ID는 필수입니다.")
-        Long clothesId
+        Long clothesId,
+
+        // baseDate 기준 값
+        LocalDateTime wornAt
 ) {
 }

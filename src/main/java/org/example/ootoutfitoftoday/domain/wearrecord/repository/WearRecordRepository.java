e2a13e1d2f9700ff96b1ce3a72cbae8596daaed1
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-09 19:07:10 +0900

    [feat] wearRecord: 소유한 옷에 대한 착용 기록을 하루에 한 번만 가능하도록 쿼리 메서드 구현

--- src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/repository/WearRecordRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/repository/WearRecordRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+
 public interface WearRecordRepository extends JpaRepository<WearRecord, Long>, WearCustomRepository {
 
     @Query(value = "SELECT DISTINCT wr FROM WearRecord wr " +
@@ -19,5 +21,22 @@ public interface WearRecordRepository extends JpaRepository<WearRecord, Long>, W
     Page<WearRecord> findMyWearRecordsWithClothes(
             @Param("userId") Long userId,
             Pageable pageable
+    );
+
+    // 기준일에 이미 등록을 했다면 예외처리
+    @Query("""
+            SELECT wr
+            FROM WearRecord wr
+            WHERE wr.user.id = :userId
+              AND wr.clothes.id = :clothesId
+              AND wr.clothes.isDeleted = false
+              AND wr.wornAt >= :startOfDay
+              AND wr.wornAt < :endOfDay
+            """)
+    boolean existsByUserIdAndClothesIdAndWornAtBetween(
+            @Param("userId") Long userId,
+            @Param("clothesId") Long clothsId,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay
     );
 }

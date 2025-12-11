e7d61160bdf0f728523a4f75b50a5216b7365afc
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-11 11:39:31 +0900

    [feat] wearRecord: 착용 기록 등록
    - 유저가 원하는 날짜에 등록할 수 있게 코드 수정
    - 다만, 유저가 api 접근일 기준으로 한달 전까지 가능하고 미래는 불가능하도록 제한을 둠.
    - 같은 날에 같은 옷은 중복으로 등록 불가능하게 제약을 걸어둠.

--- src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/service/command/WearRecordCommandServiceImpl.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/service/command/WearRecordCommandServiceImpl.java
@@ -16,6 +16,7 @@ import org.example.ootoutfitoftoday.domain.wearrecord.repository.WearRecordRepos
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -36,7 +37,26 @@ public class WearRecordCommandServiceImpl implements WearRecordCommandService {
 
         Clothes clothes = clothesQueryService.findClothesByIdAndUserIdAndIsDeletedFalse(userId, request.clothesId());
 
-        LocalDateTime wornAt = LocalDateTime.now();
+        // 입력 받은 시각이 있다면 그 시각으로 없다면 api를 접근한 시각으로 등록
+        LocalDate wornDate = request.wornDate() == null ? LocalDate.now() : request.wornDate();
+
+        // LocalDate를 LocalDateTime으로 변환
+        LocalDateTime wornAt = wornDate.atStartOfDay();
+
+        // 미래 혹은 한달 이전 검증 로직
+        validateWornAt(wornAt);
+
+        LocalDateTime startOfDay = wornDate.atStartOfDay(); // 입력된 날의 00시 00분 00초
+        LocalDateTime endOfDay = wornDate.plusDays(1).atStartOfDay(); // 입력된 날의 다음날 00시 00분 00초
+
+        // 기준일에 이미 등록을 했다면 예외처리
+        validateDuplicateWearRecord(
+                userId,
+                request.clothesId(),
+                startOfDay,
+                endOfDay
+        );
+
         WearRecord wearRecord = WearRecord.create(user, clothes, wornAt);
         WearRecord savedRecord = wearRecordRepository.save(wearRecord);
 
@@ -47,5 +67,39 @@ public class WearRecordCommandServiceImpl implements WearRecordCommandService {
         log.debug("옷 마지막 착용 일시 업데이트 완료 - 옷 ID: {}", request.clothesId());
 
         return WearRecordCreateResponse.from(savedRecord.getId());
+    }
+
+    // 미래 혹은 한달 이전 검증 로직
+    private void validateWornAt(LocalDateTime wornAt) {
+        LocalDateTime now = LocalDateTime.now();
+
+        // 미래는 예외를 던짐
+        if (wornAt.isAfter(now)) {
+            throw new WearRecordException(WearRecordErrorCode.WORN_AT_IN_FUTURE);
+        }
+
+        if (wornAt.isBefore(now.minusMonths(1))) {
+            throw new WearRecordException(WearRecordErrorCode.WORN_AT_OUT_OF_RANGE);
+        }
+    }
+
+    // 기준일에 이미 등록을 했다면 예외처리
+    private void validateDuplicateWearRecord(
+            Long userId,
+            Long clothesId,
+            LocalDateTime startOfDay,
+            LocalDateTime endOfDay
+    ) {
+
+        boolean exists = wearRecordRepository.existsByUserIdAndClothesIdAndWornAtBetween(
+                userId,
+                clothesId,
+                startOfDay,
+                endOfDay
+        );
+
+        if (exists) {
+            throw new WearRecordException(WearRecordErrorCode.DUPLICATE_WEAR_RECORD_SAME_DAY);
+        }
     }
 }

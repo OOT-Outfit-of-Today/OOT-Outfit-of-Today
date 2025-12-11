24c6713169d4c43fa7b620bb24e321747d795034
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-11 11:50:15 +0900

    [refactor] wearRecord: 불필요한 쿼리 수정

--- src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/repository/WearRecordRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/wearrecord/repository/WearRecordRepository.java
@@ -11,21 +11,12 @@ import java.time.LocalDateTime;
 
 public interface WearRecordRepository extends JpaRepository<WearRecord, Long>, WearCustomRepository {
 
-    @Query(
-            value = """
-                    SELECT DISTINCT wr
-                    FROM WearRecord wr
-                    JOIN FETCH wr.user u
-                    JOIN FETCH wr.clothes c
-                    LEFT JOIN FETCH c.images ci
-                    LEFT JOIN FETCH ci.image i
-                    WHERE u.id = :userId
-                    """,
-            countQuery = """
-                     SELECT count(wr)
-                     FROM WearRecord wr
-                     WHERE wr.user.id = :userId
-                    """)
+    @Query("""
+            SELECT wr
+            FROM WearRecord wr
+            JOIN FETCH wr.clothes c
+            WHERE wr.user.id = :userId
+            """)
     Page<WearRecord> findMyWearRecordsWithClothes(
             @Param("userId") Long userId,
             Pageable pageable

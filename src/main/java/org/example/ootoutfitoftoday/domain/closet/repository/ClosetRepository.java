21fd6239e48b91d7651dc21043a91be272cecdc7
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-16 11:03:00 +0900

    [refactor] closet: N+1 문제 해결
    - JOIN FETCH를 사용해서 문제 해결

--- src/main/java/org/example/ootoutfitoftoday/domain/closet/repository/ClosetRepository.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/closet/repository/ClosetRepository.java
@@ -12,39 +12,71 @@ import java.util.Optional;
 public interface ClosetRepository extends JpaRepository<Closet, Long> {
 
     // 내 옷장 조회용(공개 비공개 모두 조회)
-    @Query("""
-            SELECT c
-            FROM Closet c
-            WHERE c.user.id = :userId
-              AND c.isDeleted = false
-            """)
+    @Query(
+            value = """
+                    SELECT c
+                    FROM Closet c
+                    LEFT JOIN FETCH c.closetImage ci
+                    LEFT JOIN FETCH ci.image
+                    WHERE c.user.id = :userId
+                      AND c.isDeleted = false
+                    """,
+            countQuery = """
+                    SELECT count(c)
+                    FROM Closet c
+                    WHERE c.user.id = :userId
+                      AND c.isDeleted = false
+                    """)
     Page<Closet> findAllMyClosetsAndIsDeletedFalse(@Param("userId") Long userId, Pageable pageable);
 
     // 해당 유저의 공개된 옷장
-    @Query("""
-            SELECT c
-            FROM Closet c
-            WHERE c.user.id = :userId
-              AND c.isDeleted = false
-              AND c.user.isDeleted = false
-              AND c.isPublic = true
-            """)
+    @Query(
+            value = """
+                    SELECT c
+                    FROM Closet c
+                    LEFT JOIN FETCH c.closetImage ci
+                    LEFT JOIN FETCH ci.image
+                    WHERE c.user.id = :userId
+                      AND c.isDeleted = false
+                      AND c.user.isDeleted = false
+                      AND c.isPublic = true
+                    """,
+            countQuery = """
+                    SELECT count(c)
+                    FROM Closet c
+                    WHERE c.user.id = :userId
+                      AND c.isDeleted = false
+                      AND c.user.isDeleted = false
+                      AND c.isPublic = true
+                    """)
     Page<Closet> findAllPublicClosetsByUserIdAndIsDeletedFalse(@Param("userId") Long userId, Pageable pageable);
 
     // 공개된 옷장 전체 조회
-    @Query("""
-            SELECT c
-            FROM Closet c
-            WHERE c.isDeleted = false
-              AND c.user.isDeleted = false
-              AND c.isPublic = true
-            """)
+    @Query(
+            value = """
+                    SELECT c
+                    FROM Closet c
+                    LEFT JOIN FETCH c.closetImage ci
+                    LEFT JOIN FETCH ci.image
+                    WHERE c.isDeleted = false
+                      AND c.user.isDeleted = false
+                      AND c.isPublic = true
+                    """,
+            countQuery = """
+                    SELECT count(c)
+                    FROM Closet c
+                    WHERE c.isDeleted = false
+                      AND c.user.isDeleted = false
+                      AND c.isPublic = true
+                    """)
     Page<Closet> findAllPublicClosetsAndIsDeletedFalse(Pageable pageable);
 
     // 내 옷장 상세 조회
     @Query("""
             SELECT c
             FROM Closet c
+            LEFT JOIN FETCH c.closetImage ci
+            LEFT JOIN FETCH ci.image
             WHERE c.user.id = :userId
               AND c.isDeleted = false
               AND c.id = :closetId
@@ -55,6 +87,8 @@ public interface ClosetRepository extends JpaRepository<Closet, Long> {
     @Query("""
             SELECT c
             FROM Closet c
+            LEFT JOIN FETCH c.closetImage ci
+            LEFT JOIN FETCH ci.image
             WHERE c.isDeleted = false
               AND c.user.isDeleted = false
               AND c.isPublic = true
@@ -66,6 +100,8 @@ public interface ClosetRepository extends JpaRepository<Closet, Long> {
     @Query("""
             SELECT c
             FROM Closet c
+            LEFT JOIN FETCH c.closetImage ci
+            LEFT JOIN FETCH ci.image
             WHERE c.isDeleted = false
               AND c.id = :closetId
               AND c.user.id = :userId

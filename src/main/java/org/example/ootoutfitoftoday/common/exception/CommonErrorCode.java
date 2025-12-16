6add4bf07e0c94ede5dc95d4f2fc225eacef1242
cjn404 <moi404@naver.com>
2025-12-16 15:53:07 +0900

    [feat] all: CommonErrorCode에 INVALID_REQUEST_BODY 에러 코드 추가
    - 글로벌 예외 처리 핸들러 내 HttpMessageNotReadableException 핸들러에 사용

--- src/main/java/org/example/ootoutfitoftoday/common/exception/CommonErrorCode.java
+++ src/main/java/org/example/ootoutfitoftoday/common/exception/CommonErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     UNEXPECTED_SERVER_ERROR("UNEXPECTED_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "예상하지 못한 서버 오류가 발생했습니다."),
-    VALIDATION_ERROR("VALIDATION_ERROR", HttpStatus.BAD_REQUEST, "Validation 오류가 발생했습니다.");
+    VALIDATION_ERROR("VALIDATION_ERROR", HttpStatus.BAD_REQUEST, "Validation 오류가 발생했습니다."),
+    INVALID_REQUEST_BODY("INVALID_REQUEST_BODY", HttpStatus.BAD_REQUEST, "요청 데이터 형식이 올바르지 않습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;

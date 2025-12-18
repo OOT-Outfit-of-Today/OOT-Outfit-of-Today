520178c80564aa04cedcbcc2d04c2fa780cb2ffd
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-12-18 11:10:00 +0900

    [feat] clothesImage: patch용 response 추가

---
+++ src/main/java/org/example/ootoutfitoftoday/domain/clothesImage/dto/response/ClothesImageChangeMainResponse.java
@@ -0,0 +1,28 @@
+package org.example.ootoutfitoftoday.domain.clothesImage.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ClothesImageChangeMainResponse {
+
+    private final Long userId;
+    private final Long clothesId;
+    private final ClothesImageResponse clothesImages;
+
+    @Builder
+    public ClothesImageChangeMainResponse(Long userId, Long clothesId, ClothesImageResponse clothesImages) {
+        this.userId = userId;
+        this.clothesId = clothesId;
+        this.clothesImages = clothesImages;
+    }
+
+    public static ClothesImageChangeMainResponse from(Long userId, Long clothesId, ClothesImageResponse clothesImages) {
+
+        return ClothesImageChangeMainResponse.builder()
+                .userId(userId)
+                .clothesId(clothesId)
+                .clothesImages(clothesImages)
+                .build();
+    }
+}

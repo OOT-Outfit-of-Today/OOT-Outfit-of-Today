086b7ea17b623ad8ebf835a57b241f2f7dc543ac
cjn404 <moi404@naver.com>
2025-12-22 14:06:15 +0900

    [fix] salePostImage: Swagger RequestBody import로 인한 요청 본문 바인딩 실패 해결
    - 잘못된 import로,
    - 실제 요청 본문 데이터가 바인딩되지 않아 request 객체가 null 상태
    - @Valid 검증 시 @NotEmpty 조건에 걸려 400 Validation 에러 발생

--- src/main/java/org/example/ootoutfitoftoday/domain/salepostimage/controller/SalePostImageController.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/salepostimage/controller/SalePostImageController.java
@@ -1,7 +1,6 @@
 package org.example.ootoutfitoftoday.domain.salepostimage.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;

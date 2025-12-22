ddf15b9c38def2c91d83289710a8f892f5b26450
cjn404 <moi404@naver.com>
2025-12-23 00:38:02 +0900

    [docs] location: MySQL SRID 4326 좌표 순서 관련 주석 추가
    - MySQL SRID 4326은 POINT(위도, 경도) 순서를 사용
    - WKT 표준 POINT(경도, 위도)와 다름에 유의
    - format(): POINT(latitude, longitude) 순서로 생성
    - parse(): location[0]=위도, location[1]=경도로 파싱
    - 주석 추가로 혼동 방지

--- src/main/java/org/example/ootoutfitoftoday/common/util/PointFormatAndParse.java
+++ src/main/java/org/example/ootoutfitoftoday/common/util/PointFormatAndParse.java
@@ -4,9 +4,16 @@ import java.math.BigDecimal;
 
 public class PointFormatAndParse {
 
+    // WKT 표준 순서인 POINT(경도, 위도)와 달리
+    // MySQL SRID 4326은 POINT(위도, 경도) 순서
     public static String format(BigDecimal tradeLatitude, BigDecimal tradeLongitude) {
 
-        return String.format("POINT(%s %s)", tradeLatitude, tradeLongitude);
+        String result = String.format("POINT(%s %s)",
+                tradeLatitude,    // 위도(latitude) 먼저
+                tradeLongitude    // 경도(longitude) 나중
+        );
+
+        return result;
     }
 
     public static Location parse(String tradeLocation) {
@@ -14,8 +21,8 @@ public class PointFormatAndParse {
 
         String[] location = coordinateLocation.split(" ");
 
-        String latitudeStr = location[0];
-        String longitudeStr = location[1];
+        String latitudeStr = location[0];     // 위도
+        String longitudeStr = location[1];    // 경도
 
         BigDecimal tradeLatitude = new BigDecimal(latitudeStr);
         BigDecimal tradeLongitude = new BigDecimal(longitudeStr);

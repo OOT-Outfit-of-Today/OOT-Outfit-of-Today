4a5fd512c8d0086f52c6e6d65531fef1bd1b2dbd
cjn404 <moi404@naver.com>
2026-01-07 20:11:57 +0900

    [feat] auth: AuthException에 중복 필드 전달 생성자 추가

--- src/main/java/org/example/ootoutfitoftoday/domain/auth/exception/AuthException.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/auth/exception/AuthException.java
@@ -2,6 +2,8 @@ package org.example.ootoutfitoftoday.domain.auth.exception;
 
 import org.example.ootoutfitoftoday.common.exception.GlobalException;
 
+import java.util.List;
+
 public class AuthException extends GlobalException {
 
     public AuthException(AuthErrorCode authErrorCode) {
@@ -10,5 +12,10 @@ public class AuthException extends GlobalException {
 
     public AuthException(AuthErrorCode authErrorCode, AuthSuccessCode authSuccessCode) {
         super(authErrorCode, authSuccessCode);
+    }
+
+    // 중복 필드 전달
+    public AuthException(AuthErrorCode authErrorCode, List<String> duplicateFields) {
+        super(authErrorCode, duplicateFields);
     }
 }

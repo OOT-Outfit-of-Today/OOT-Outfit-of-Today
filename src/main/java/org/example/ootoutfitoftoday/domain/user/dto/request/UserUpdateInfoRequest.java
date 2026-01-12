84568a1b8db4312dbe12497c58988ac80ee7b162
cjn404 <moi404@naver.com>
2026-01-12 22:35:48 +0900

    [refactor] user: UserUpdateInfoRequest에서 불필요한 임포트문 제거

--- src/main/java/org/example/ootoutfitoftoday/domain/user/dto/request/UserUpdateInfoRequest.java
+++ src/main/java/org/example/ootoutfitoftoday/domain/user/dto/request/UserUpdateInfoRequest.java
@@ -1,7 +1,6 @@
 package org.example.ootoutfitoftoday.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
